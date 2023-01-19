97d0d4bbd1c39f3f5c06a4781c822bf1feb89679
roman <romantisenko3@gmail.com>
2023-01-20 01:43:08 +0400

    carousel

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/arrow_left.svg"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/carousel/arrow_right.svg"></img></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {                   
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {                   
+                    dots: true,
+                    arrows: false
+                }
+            },         
+        ]     
+    });
+}); 
